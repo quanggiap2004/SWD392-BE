04394683e9277d51b7ff978e73238702d760b6a4
phonglam <lamphong1507@gmail.com>
2025-02-18 17:56:27 +0700

    commit to boxImage

---
+++ Application/Implementations/BoxImageService.cs
@@ -0,0 +1,48 @@
+﻿using BlindBoxSystem.Application.Interfaces;
+using BlindBoxSystem.Data.Interfaces;
+using BlindBoxSystem.Domain.Entities;
+using BlindBoxSystem.Domain.Model.BoxImageDTOs;
+
+namespace BlindBoxSystem.Application.Implementations
+{
+    public class BoxImageService : IBoxImageService
+    {
+
+        private readonly IBoxImageRepository _boxImageRepository;
+        public BoxImageService(IBoxImageRepository boxImageRepository)
+        {
+            _boxImageRepository = boxImageRepository;
+        }
+
+        public async Task<BoxImage> AddBoxImageAsync(BoxImage boxImage)
+        {
+            var addedBoxImage = await _boxImageRepository.AddBoxImageAsync(boxImage);
+            return addedBoxImage;
+        }
+
+        public Task DeleteBoxImageAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetAllBoxImageDTO>> GetAllBoxesImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BoxImage> GetBoxImageById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetAllBoxImageDTO> GetBoxImageDTO(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BoxImage> UpdateBoxImageAsync(int id, BoxImage boxImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Model/BoxImageDTOs/GetAllBoxImageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BlindBoxSystem.Domain.Model.BoxImageDTOs
+{
+    public class GetAllBoxImageDTO
+    {
+        public int BoxImageId { get; set; }
+        public string BoxImageUrl { get; set; }
+        public int BoxId { get; set; }
+        public string BoxName { get; set; }
+    }
+}
