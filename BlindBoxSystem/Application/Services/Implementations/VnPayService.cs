957eae9ef09ce1008915364ca8f206a5a0b18908
quanggiap2004 <nguyenquanggiap642004@gmail.com>
2025-02-25 23:16:31 +0700

    fix vnpay amount error

--- BlindBoxSystem/Application/Services/Implementations/VnPayService.cs
+++ BlindBoxSystem/Application/Services/Implementations/VnPayService.cs
@@ -28,7 +28,7 @@ namespace BlindBoxSystem.Application.Services.Implementations
             pay.AddRequestData("vnp_CurrCode", _configuration["Vnpay:CurrCode"]);
             pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(context));
             pay.AddRequestData("vnp_Locale", _configuration["Vnpay:Locale"]);
-            pay.AddRequestData("vnp_OrderInfo", $"{orderId}");
+            pay.AddRequestData("vnp_OrderInfo", $"{orderId.ToString()}");
             pay.AddRequestData("vnp_OrderType", "Blindbox");
             pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
             pay.AddRequestData("vnp_TxnRef", tick);
