0cc945bc0bafcc88ae7e54e35a4133d64a8ac559
quanggiap <nguyenquanggiap642004@gmail.com>
2025-03-16 17:26:05 +0700

    fix refund api bug

--- Data/Repository/Implementations/OrderItemRepository.cs
+++ Data/Repository/Implementations/OrderItemRepository.cs
@@ -47,35 +47,41 @@ namespace Data.Repository.Implementations
             };
         }
 
-        public async Task<bool> UpdateDataAfterRefund(RefundOrderItemRequestDto request, int id)
+        public async Task<bool> UpdateDataAfterRefund(RefundOrderItemRequestDto request, int orderItemId)
         {
-            var result = await _context.OrderItems.Include(oi => oi.Order).FirstOrDefaultAsync(x => x.OrderItemId == id);
-            if (result == null)
+            // Retrieve the order item with its associated order.
+            var orderItem = await _context.OrderItems
+                .Include(oi => oi.Order)
+                .FirstOrDefaultAsync(x => x.OrderItemId == orderItemId);
+
+            if (orderItem == null)
             {
                 return false;
             }
-            if(result.Quantity < request.numOfRefund)
+
+            if (orderItem.Quantity < request.numOfRefund)
             {
                 throw new Exception("Refund quantity is greater than the quantity of the order item");
             }
-            var newProfit = result.Order.Revenue - request.numOfRefund * result.OrderPrice;
-            var orderItemListOfOrderId = await _context.OrderItems.Where(x => x.OrderId == result.OrderId).ToListAsync();
-            bool refundRequestCheck = false;
-            foreach (var orderItem in orderItemListOfOrderId)
-            {
-                if(orderItem.RefundStatus == ProjectConstant.RefundRequest)
-                {
-                    refundRequestCheck = true;
-                    break;
-                }
-            }
-            result.NumOfRefund = request.numOfRefund;
-            result.Order.RefundRequest = refundRequestCheck;
-            result.Order.Revenue = newProfit;
-            result.RefundStatus = ProjectConstant.RefundResolved;
+
+            orderItem.NumOfRefund = request.numOfRefund;
+            orderItem.RefundStatus = ProjectConstant.RefundResolved;
+
+            await _context.SaveChangesAsync();
+
+            var newRevenue = orderItem.Order.Revenue - request.numOfRefund * orderItem.OrderPrice;
+
+            bool hasPendingRefunds = await _context.OrderItems
+                .Where(x => x.OrderId == orderItem.OrderId && x.OrderItemId != orderItem.OrderItemId)
+                .AnyAsync(x => x.RefundStatus == ProjectConstant.RefundRequest);
+
+            orderItem.Order.RefundRequest = hasPendingRefunds;
+            orderItem.Order.Revenue = newRevenue;
+
             await _context.SaveChangesAsync();
             return true;
         }
+
 
         public async Task<bool> UpdateRefundRequest(int id)
         {
