2775b5698e1dbd3b1b69a90861a98f1f84e1f9b4
quanggiap <nguyenquanggiap642004@gmail.com>
2025-03-26 13:07:33 +0700

    update database to change datatype of average rating

---
+++ Domain/Domain/Migrations/20250326060555_change column average rating to float.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlindBoxSystem.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class changecolumnaverageratingtofloat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "AverageRating",
+                table: "BoxItems",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AverageRating",
+                table: "BoxItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
